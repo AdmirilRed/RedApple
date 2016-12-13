9ff250265e8580a8c604a7688822ad1657d9893b
Joseph Manahan <Joseph.Manahan@utexas.edu>
2016-12-13 16:09:40 -0600

    Initialization

---
+++ src/main/java/net/commstationfive/redapple/RedApple.java
@@ -0,0 +1,15 @@
+package net.commstationfive.redapple;
+
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "redapple", name = "RedApple", version = "1.0")
+public class RedApple {
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        // Hey! The server has started!
+        // Try instantiating your logger in here.
+        // (There's a guide for that)
+    }
+}
