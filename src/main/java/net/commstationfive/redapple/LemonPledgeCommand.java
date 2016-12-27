3a0d9f85625d5ef966e305624299e5b7afc9bab2
Joseph Manahan <Joseph.Manahan@utexas.edu>
2016-12-27 11:36:54 -0600

    LemonPledge Command Class

---
+++ src/main/java/net/commstationfive/redapple/LemonPledgeCommand.java
@@ -0,0 +1,40 @@
+package net.commstationfive.redapple;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import com.google.inject.Inject;
+
+public class LemonPledgeCommand implements CommandExecutor {
+	
+	@Inject
+	private static final Logger logger = LoggerFactory.getLogger(LemonPledgeCommand.class);
+
+	public CommandResult execute(CommandSource src, CommandContext args) throws
+	CommandException {
+		
+		if(src instanceof Player) {
+			
+			Player player = (Player) src;
+			player.sendMessage(Text.of(player.getName() + " demands more lemon pledge!"));		
+		} else {
+			
+			src.sendMessage(Text.of("The gods demand more lemon pledge!"));	
+		}
+		logger.debug("Demand for lemon pledge from source: " + src);
+		
+        return CommandResult.success();
+	}
+	
+	public Logger getLogger() {
+		
+		return logger;
+	}
+
+}
